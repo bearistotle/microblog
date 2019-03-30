fc1467a657a968cf491c519070c8ba2b14fd1509
Brandon Walker <brandonwalker215@gmail.com>
2019-03-30 16:04:59 -0500

    added shell context func to microblog.py

--- microblog.py
+++ microblog.py
@@ -1,3 +1,6 @@
-from app import app  # note two tokens of 'app' are diff vars
-                     # the first is the package, the second is
-                     # the var created in the __init__.py file
+from app import app, db                # note two tokens of 'app' are diff vars
+from app.models import User, Post      # the first is the package, the second is
+                                       # the var created in the __init__.py file
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
